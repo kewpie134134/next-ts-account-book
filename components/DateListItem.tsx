f9069526d1508c683774940803d5303cbbf218fd
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-04-15 22:12:58 +0900

    日付を選択してページ遷移を実施

---
+++ components/DateListItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+import { SampleDate } from "../interfaces";
+
+type Props = {
+  data: SampleDate;
+};
+
+const ListItem = ({ data }: Props) => (
+  <Link href="/account-book/[id]" as={`/account-book/${data.date}`}>
+    <a>{data.date}</a>
+  </Link>
+);
+
+export default ListItem;
