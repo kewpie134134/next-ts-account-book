a66afba14fe9728bdb8753a50b17fdd547c05734
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-04-17 12:04:06 +0900

    リファクタリング

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,48 +7,44 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => {
-  const motherPath: string = `/mother/${getThisMonth()}`;
-  const fatherPath: string = `/father/${getThisMonth()}`;
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <header>
-        <nav>
-          <Link href="/">
-            <a>Home</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/about">
-            <a>About</a>
-          </Link>{' '}
-          |{' '}
-          <Link href={motherPath}>
-            <a>Mother</a>
-          </Link>{' '}
-          |{' '}
-          <Link href={fatherPath}>
-            <a>Father</a>
-          </Link>{' '}
-          |{' '}
-          <Link href="/account-book">
-            <a>Account book</a>
-          </Link>{' '}
-          | <a href="/api/users">Users API</a>
-        </nav>
-      </header>
-      {children}
-      <footer>
-        <hr />
-        <span>I'm here to stay (Footer)</span>
-      </footer>
-    </div>
-  );
-};
+const Layout = ({ children, title = 'This is the default title' }: Props) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{' '}
+        |{' '}
+        <Link href="/about">
+          <a>About</a>
+        </Link>{' '}
+        |{' '}
+        <Link href={`/mother/${getThisMonth()}`}>
+          <a>Mother</a>
+        </Link>{' '}
+        |{' '}
+        <Link href={`/father/${getThisMonth()}`}>
+          <a>Father</a>
+        </Link>{' '}
+        |{' '}
+        <Link href="/account-book">
+          <a>Account book</a>
+        </Link>{' '}
+        | <a href="/api/users">Users API</a>
+      </nav>
+    </header>
+    {children}
+    <footer>
+      <hr />
+      <span>I'm here to stay (Footer)</span>
+    </footer>
+  </div>
+);
 
 const getThisMonth = (): string => {
   const today = new Date();
