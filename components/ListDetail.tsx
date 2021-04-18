da94bc1fd6e17f106d229d1f32959ae679317660
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-04-18 10:58:49 +0900

    メインコンポーネントのほうでfirestoreの値を取得することができた

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Link from 'next/link';
 
 import { User } from '../interfaces';
+import { db } from '../utils/Firebase';
 
 type ListDetailProps = {
   item: User;
@@ -10,6 +11,18 @@ type ListDetailProps = {
 };
 
 const ListDetail = ({ item, id: path, user }: ListDetailProps) => {
+  const data: any = [];
+  db.collection('father-2021-04')
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        data.push({
+          date: doc.data().date,
+          item: doc.data().item
+        });
+      });
+    });
+  console.log(data);
   return (
     <div>
       <h1>Detail for {item.name}</h1>
